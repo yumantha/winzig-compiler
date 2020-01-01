fc24b7bd0b79ea797d7513de72c9903222178c10
Yumantha <yumanthadesilva@gmail.com>
2020-01-01 17:41:33 +0530

    minor changes and input folder

--- src/com/yumantha/Main.java
+++ src/com/yumantha/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String inputPath = "winzig_test_programs/";
+        String inputPath = "input/";
         String outputPath = "output/";
 
         for (int i = 1; i <= 15; i++) {
@@ -18,6 +18,8 @@ public class Main {
 
             String inFile = inputPath + "winzig_" + iStr;
             String outFile = outputPath + "winzig_" + iStr + ".tree";
+
+            System.out.println(inFile);
 
             Compiler.compile(inFile, outFile);
             System.out.println("------------------------------------------------------------");
